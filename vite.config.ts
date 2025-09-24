2ec9f3f9f11047bf221e5a39fedf82eb4860aaac
shubham sagar <115171173+shubhamasagar@users.noreply.github.com>
2025-09-24 13:57:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Add this line inside the defineConfig object
+  base: "/portfolio/", 
   server: {
     host: "::",
     port: 8080,
